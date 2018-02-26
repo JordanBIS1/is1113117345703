10b8f6b429facb5be08a12635be5bbec86fbbd0a
Jordan O'Connor <117345703@umail.ucc.ie>
2018-02-26 16:04:57 +0000

    Pushing to GitHub

--- ebusiness/Ebus3.php
+++ ebusiness/Ebus3.php
@@ -6,9 +6,8 @@
     <body>
         <h4>RECEIPT</h4>
                 
-        <?php
-        // Echo session variables that were set on previous page
-        echo "Total is " . $_SESSION["total"] . ".";
-        ?>
+        
+       
+        
     </body>
 </html>
